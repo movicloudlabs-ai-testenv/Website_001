cbf9a87e2c18b40c83416b52e16a9d2278456ef2
sanjitsriram08 <sanjit@kr-dx.com>
2025-11-25 15:35:57 +0530

    hash routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 // Pages
 import Index from "./pages/Index";
@@ -35,7 +35,7 @@ const App = () => (
       <Toaster />
       <Sonner />
 
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           {/* Home */}
           <Route path="/" element={<Index />} />
@@ -58,7 +58,7 @@ const App = () => (
           {/* 404 Page */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
